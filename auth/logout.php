895cb11cefb9ed81f7089da9bbf1d662a94b24f2
Asepp1 <asepnurhotam18@gmail.com>
2025-06-01 23:34:52 +0700

    Clean main branch

--- auth/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../config/connect.sample.php';
-require_once '../config/function.php';
-session_unset();
-session_destroy();
-
-echo '<script>window.location="' . base_url('../') . '";</script>';
-exit();
